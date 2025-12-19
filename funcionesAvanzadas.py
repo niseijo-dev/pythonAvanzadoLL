75738511378f2cea99c169c3f881bf7ccdf33ad4
Nicolas-Seijo <niseijo@uade.edu.ar>
2025-12-19 15:18:39 -0300

    Finalice el archivo 'funcionesAvanzadas.py' en el cual se explora como se utilizan los argumentos *args y **kwargs así como un repaso de las funciones lambda

--- funcionesAvanzadas.py
+++ funcionesAvanzadas.py
@@ -1,7 +1,8 @@
+"""USO DE *args"""
 def calc_perimetro(*args):
     perim = 0
     # Puedo imprimir args para ver que contiene
-    print("-- '*args' =", args)
+    #print("-- '*args' =", args)
 
     # Se puede iterar *args para utilizar cada parámetro
     for lado in args:
@@ -9,6 +10,42 @@ def calc_perimetro(*args):
     
     return perim
 
+rectangulo = calc_perimetro(1,2,3,4) # Se envía de esta forma y la función recibe una tupla con los valores enviados
+print("-- Perimetro del cuadrado =", rectangulo)
+print("")
 
-perimetro = calc_perimetro(1,2,3,4) # Se envía de esta forma y la función recibe una tupla con los valores enviados
-print("-- Perimetro =", perimetro)
+"""USO DE **kwargs"""
+def func_kwargs(**kwargs):
+    # Los parámetros se envían como "llave = valor" y kwargs los recibe como un diccionario
+    print("++",kwargs)
+
+    # Podemos iterarlo con un foreach de la siguiente forma
+    for key, value in kwargs.items():
+        print(f"++ K: {key}, V: {value}")
+    
+    # Como recibe los parámetros como un diccionario, puede usarse de la misma manera que uno cualquiera
+    print("++",kwargs["nombre"], kwargs["apellido"])
+
+func_kwargs(nombre = "Juan", apellido = "Pérez")
+
+"""USO COMBINADO DE PARÁMETROS"""
+# Primero se ponen los parámetros (normales), después *args y por último **kwargs
+def param_ordenados(nombre, apellido, *args, **kwargs):
+    print("No hay errores porque los parámetros están en orden")
+
+"""
+def param_desordenados(nombre, apellido, **kwargs, *args):
+    print("Esta línea no se ejecutará porque dará error")
+"""
+
+param_ordenados("Nicolas", "Seijo") #*args y **kwargs son opcionales
+
+"""FUNCIONES LAMBDA"""
+# Son funciones anónimas y su sintaxis es: lambda argumentos: expresion
+print ("## 2 * 2 es igual a:", (lambda x: x*2)(2)) # De esta forma la llamo y mando un parámetro directamente
+producto = lambda num, mult: num * mult # De esta forma la asigno a una variable para reutilizarla
+print("## 5 * 3 es igual a:", producto(5,3)) 
+
+# Las funciones lambda pueden ser algo dificiles de leer, pero se recomienda su uso para enviarse como parametro en funciones como filter() o map()
+
+
